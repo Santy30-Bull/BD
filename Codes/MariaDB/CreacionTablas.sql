c6969ad077d2f50bcac8d4d281407677d5daee7a
Dduquejh <johandanielduquehernandez@gmail.com>
2024-05-22 22:05:42 -0500

    Código para modificar las tablas y correción en creación

--- Codes/MariaDB/CreacionTablas.sql
+++ Codes/MariaDB/CreacionTablas.sql
@@ -52,11 +52,12 @@ CREATE TABLE RevistaCientifica (
   NoEjemplares INT,
   FechaPublicacion DATE,
   Genero VARCHAR(20) NOT NULL,
-  NoPaginas INT
+  Tematica LONGTEXT NOT NULL,
+  Periocidad VARCHAR(15)
 );
 
 CREATE TABLE InformeInvestigacion (
-  ID_Informe VARCHAR(36) NOT NULL PRIMARY KEY,
+  ID_Informe INT AUTO_INCREMENT PRIMARY KEY,
   Tipo VARCHAR(7) NOT NULL,
   Titulo VARCHAR(40) NOT NULL,
   Autor VARCHAR(20) NOT NULL,
@@ -66,7 +67,7 @@ CREATE TABLE InformeInvestigacion (
 );
 
 CREATE TABLE Sede (
-  ID_Sede VARCHAR(36) NOT NULL PRIMARY KEY,
+  ID_Sede CHAR(36) PRIMARY KEY DEFAULT (UUID()),
   Localidad VARCHAR(50) NOT NULL,
   ID_Biblioteca INT,
   FOREIGN KEY (ID_Biblioteca) REFERENCES Biblioteca (ID_Biblioteca) ON DELETE CASCADE
@@ -84,7 +85,7 @@ CREATE TABLE Calificacion (
    PRIMARY KEY(ID_Calificacion,ID_Estudiante,ID_Curso,TypeID_Estudiante),
    FOREIGN KEY (TypeID_Estudiante, ID_Estudiante) REFERENCES Estudiante (TypeID_Estudiante, ID_Estudiante) ON DELETE CASCADE,
 	FOREIGN KEY (ID_Curso) REFERENCES Curso (ID_Curso) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE Certificado (
 	TypeID_Estudiante VARCHAR(10),
@@ -94,7 +95,7 @@ CREATE TABLE Certificado (
 	PRIMARY KEY (TypeID_Estudiante,ID_Estudiante,ID_Curso),
 	FOREIGN KEY (TypeID_Estudiante,ID_Estudiante) REFERENCES Estudiante (TypeID_Estudiante,ID_Estudiante),
 	FOREIGN KEY (ID_Curso) REFERENCES Curso (ID_Curso)
-)
+);
 
 CREATE TABLE Horarios (
 	ID_Horario INT AUTO_INCREMENT,
@@ -103,16 +104,16 @@ CREATE TABLE Horarios (
 	Hora VARCHAR(6) NOT NULL,
 	PRIMARY KEY (ID_Horario,ID_Curso),
 	FOREIGN KEY (ID_Curso) REFERENCES curso (ID_Curso) ON DELETE CASCADE 
-)
+);
 
 CREATE TABLE Anuncios (
 	ID_Anuncios INT AUTO_INCREMENT,
 	ID_Curso VARCHAR(10),
 	Fecha DATE NOT NULL, 
-	Mensaje LONGBLOB NOT NULL, -- Cambio de VARCHAR(MAX) a LONGBLOB
-	PRIMARY KEY (ID_Anuncios,ID_Curso)
+	Mensaje LONGTEXT NOT NULL, -- Cambio de VARCHAR(MAX) a LONGTEXT
+	PRIMARY KEY (ID_Anuncios,ID_Curso),
 	FOREIGN KEY (ID_Curso) REFERENCES curso (ID_Curso) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE Sede_Curso(
     ID_Sede VARCHAR(36),
@@ -120,5 +121,55 @@ CREATE TABLE Sede_Curso(
     PRIMARY KEY (ID_Sede,ID_Curso),
     FOREIGN KEY (ID_Sede) REFERENCES Sede (ID_Sede) ON DELETE CASCADE,
     FOREIGN KEY (ID_Curso) REFERENCES Curso (ID_Curso) ON DELETE CASCADE
-)
+);
 
+CREATE TABLE Sede_Profesor(
+	ID_Sede VARCHAR(36),
+	TypeID_Profesor VARCHAR(10),
+	ID_Profesor VARCHAR(15),
+	PRIMARY KEY (ID_Sede, TypeID_Profesor, ID_Profesor),
+	FOREIGN KEY (ID_Sede) REFERENCES sede (ID_Sede),
+	FOREIGN KEY (TypeID_Profesor, ID_Profesor) REFERENCES Profesor (TypeID_Profesor, ID_Profesor) ON DELETE CASCADE
+);
+
+CREATE TABLE Profesor_Curso(
+	TypeID_Profesor VARCHAR(10),
+	ID_Profesor VARCHAR(15),
+	ID_Curso VARCHAR(10),
+	PRIMARY KEY (ID_Curso, TypeID_Profesor, ID_Profesor),
+	FOREIGN KEY (ID_Curso) REFERENCES Curso (ID_Curso) ON DELETE CASCADE,
+	FOREIGN KEY (TypeID_Profesor, ID_Profesor) REFERENCES Profesor (TypeID_Profesor, ID_Profesor) ON DELETE CASCADE
+);
+
+CREATE TABLE Curso_Estudiante(
+	TypeID_Estudiante VARCHAR(10),
+	ID_Estudiante VARCHAR(15),
+	ID_Curso VARCHAR(10),
+	PRIMARY KEY (ID_Curso, TypeID_Estudiante, ID_Estudiante),
+	FOREIGN KEY (ID_Curso) REFERENCES Curso (ID_Curso) ON DELETE CASCADE,
+	FOREIGN KEY (TypeID_Estudiante, ID_Estudiante) REFERENCES Estudiante (TypeID_Estudiante, ID_Estudiante) ON DELETE CASCADE
+);
+
+CREATE TABLE Biblioteca_Libro(
+	ID_Biblioteca INT,
+	ID_Libro INT,
+	PRIMARY KEY (ID_Biblioteca, ID_Libro),
+	FOREIGN KEY (ID_Biblioteca) REFERENCES Biblioteca (ID_Biblioteca) ON DELETE CASCADE,
+	FOREIGN KEY (ID_Libro) REFERENCES Libro (ID_Libro) ON DELETE CASCADE
+);
+
+CREATE TABLE Biblioteca_Revista(
+	ID_Biblioteca INT,
+	ID_Revista INT,
+	PRIMARY KEY (ID_Biblioteca, ID_Revista),
+	FOREIGN KEY (ID_Biblioteca) REFERENCES Biblioteca (ID_Biblioteca) ON DELETE CASCADE,
+	FOREIGN KEY (ID_Revista) REFERENCES RevistaCientifica(ID_Revista) ON DELETE CASCADE
+);
+
+CREATE TABLE Biblioteca_Informe(
+	ID_Biblioteca INT,
+	ID_Informe INT,
+	PRIMARY KEY (ID_Biblioteca, ID_Informe),
+	FOREIGN KEY (ID_Biblioteca) REFERENCES Biblioteca (ID_Biblioteca) ON DELETE CASCADE,
+	FOREIGN KEY (ID_Informe) REFERENCES InformeInvestigacion(ID_Informe) ON DELETE CASCADE
+);
